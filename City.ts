cdc4b6dac6093c1136967a12b35d6f64212de6bf
Sandhya200307 <152253299+Sandhya200307@users.noreply.github.com>
2024-07-05 11:54:41 +0530

    City.ts

---
+++ City.ts
@@ -0,0 +1,28 @@
+
+import { Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn } from 'typeorm';
+import { Country } from './Country';
+
+@Entity()
+export class City {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  countryId: number;
+
+  @Column()
+  isActive: boolean;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  long: number;
+
+  @OneToOne(() => Country, (country) => country.id)
+  @JoinColumn()
+  country: Country;
+}
